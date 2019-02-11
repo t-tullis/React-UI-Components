c2ae56f18ab5b729bad7393f62ec4fc7dcd3c5a1
t-tullis <t.tullis34@gmail.com>
2019-02-11 16:23:59 -0500

    completed components

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () =>{
+    return(
+        <p>Lambda School @LambdaSchool  • 26 Jan</p>
+    );
+};
+
+export default HeaderTitle;
