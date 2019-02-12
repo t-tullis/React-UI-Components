5ff1285f6401858af923e2b1068f3d213202578f
t-tullis <t.tullis34@gmail.com>
2019-02-12 18:02:36 -0500

    added components and working on styling

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,21 +1,40 @@
 import React from 'react';
 import './App.css';
+// import ButtonContainer from './components/ButtonComponents/ButtonContainer'
+import NumberButton from './components/ButtonComponents/NumberButton';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className= 'calc-container'>
+      <CalculatorDisplay  />
+      <div className = 'action-btns'>
+        <ActionButton buttonStyle='red-button' text = "Clear" />
+        <NumberButton buttonStyle='number-btns' className = 'operator' text = "/" />
+      </div>
+        <div className = 'row'>
+          <NumberButton buttonStyle='number-btns' text= "7"/>
+          <NumberButton buttonStyle='number-btns' text = "8"/>
+          <NumberButton buttonStyle='number-btns' text = "9"/>
+          <NumberButton buttonStyle='red-button' text = "X" />
+      </div>
+      <div className = 'row'>
+          <NumberButton buttonStyle='number-btns' text= "4"/>
+          <NumberButton buttonStyle='number-btns' text = "5"/>
+          <NumberButton buttonStyle='number-btns' text = "6"/>
+          <NumberButton buttonStyle='red-button'  text = "-" />
+      </div>
+      <div className = 'row'>
+          <NumberButton buttonStyle='number-btns' text= "1"/>
+          <NumberButton buttonStyle='number-btns' text = "2"/>
+          <NumberButton buttonStyle='number-btns' text = "3"/>
+          <NumberButton buttonStyle='red-button' text = "+" />
+      </div>
+      <div className = 'action-btns'>
+        <ActionButton buttonStyle='number-btns' text = "0" />
+        <NumberButton buttonStyle='red-button' text = "=" />
+      </div>
     </div>
   );
 };
